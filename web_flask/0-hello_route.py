4a56b9d6d9e90b08e59ec57409fe3dbbbbaffcb9
Samtegg <137792099+Samtegg@users.noreply.github.com>
2024-05-28 03:53:11 +0100

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+This starts a Flask web application
 """
+
+
 
 from flask import Flask
 app = Flask(__name__)
@@ -11,6 +13,8 @@ app = Flask(__name__)
 def index():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
