5ed9ed257124ce8ccb2e1ce198bb9ded78215c41
Samtegg <137792099+Samtegg@users.noreply.github.com>
2024-05-28 03:57:08 +0100

    Update 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+This starts a Flask web application
 """
+
+
 
 from flask import Flask, render_template
 from models import *
 from models import storage
 app = Flask(__name__)
+
+
 
 
 @app.route('/states', strict_slashes=False)
@@ -19,10 +23,12 @@ def states(state_id=None):
     return render_template('9-states.html', states=states, state_id=state_id)
 
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
