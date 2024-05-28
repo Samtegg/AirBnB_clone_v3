0bd66ce90b2a2793d2d1500c5a295838e471ba8b
Samtegg <137792099+Samtegg@users.noreply.github.com>
2024-05-28 03:56:31 +0100

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+
 """
-starts a Flask web application
+This starts a Flask web application
 """
+
+
 
 from flask import Flask, render_template
 from models import *
@@ -9,11 +12,15 @@ from models import storage
 app = Flask(__name__)
 
 
+
+
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     """display the states and cities listed in alphabetical order"""
     states = storage.all("State").values()
     return render_template('8-cities_by_states.html', states=states)
+
+
 
 
 @app.teardown_appcontext
