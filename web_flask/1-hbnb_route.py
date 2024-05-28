597dcb8442dd1777a2a2cc65bfc9a5e409f63f81
Samtegg <137792099+Samtegg@users.noreply.github.com>
2024-05-28 03:53:29 +0100

    Update 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+This starts a Flask web application
 """
 
 from flask import Flask
 app = Flask(__name__)
+
+
 
 
 @app.route('/', strict_slashes=False)
@@ -13,10 +15,14 @@ def index():
     return 'Hello HBNB!'
 
 
+
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """returns HBNB"""
     return 'HBNB'
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
