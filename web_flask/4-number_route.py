2ea060d71079a1dfe7b7385cce464e39ce8cfbee
Samtegg <137792099+Samtegg@users.noreply.github.com>
2024-05-28 03:55:00 +0100

    Update 4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+This starts a Flask web application
 """
+
+
 
 from flask import Flask
 app = Flask(__name__)
+
+
 
 
 @app.route('/', strict_slashes=False)
@@ -13,10 +17,12 @@ def index():
     return 'Hello HBNB!'
 
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """returns HBNB"""
     return 'HBNB'
+
 
 
 @app.route('/c/<text>', strict_slashes=False)
@@ -25,11 +31,14 @@ def cisfun(text):
     return 'C ' + text.replace('_', ' ')
 
 
+
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def pythoniscool(text='is cool'):
     """display “Python ”, followed by the value of the text variable"""
     return 'Python ' + text.replace('_', ' ')
+
+
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
